84b4d2161a9ae610ca0e9d90a2134db8d41470bc
Ashar <lawa0014@umn.edu>
2013-04-08 10:53:09 -0500

    Added FEM and constants.m

--- constants.m
+++ constants.m
@@ -1,10 +1,19 @@
+<<<<<<< HEAD
 % example script
 %% Project #1
 % Authors: Adedayo Lawal and Blake Levy
 clc;clear;
+=======
+% constants.m
+%% Project #2
+% Authors: Blake Levy and Adedayo Lawal
+>>>>>>> Added FEM and constants.m
 %% Set up Parameters
 c = 299792458;
 mu = (4*pi)*1e-7;
 epsilon = 1/(mu*c^2);
+<<<<<<< HEAD
 e_top = epsilon;
 e_bottom = 3*epsilon;
+=======
+>>>>>>> Added FEM and constants.m
