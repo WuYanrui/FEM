ae9c5a636f1c5d4f50789e552649fb7c61d29850
Blake Levy <blake.a.levy@gmail.com>
2013-04-10 15:53:47 -0500

    Getting closer
    Fixed some indexing issues. Almost there

--- FEM.m
+++ FEM.m
@@ -8,31 +8,41 @@ m = 50; % Number of elements
 n = m+1; % Number of nodes
 L = 5*lamb0; % Length of slab is 5x free space wavelength
 x = 0:L/m:L; % discretize dielectric slab with M elements
+y = 0:L/(m-1):L;
 theta = 0:pi/2/m:pi/2; % incident angle range from 0 to 90 degrees
-er_slab = 4 + (2-1j*.1)*((1-x/L).^2);
+er_slab = 4 + (2-1j*.1)*((1-y/L).^2);
 e_slab = eps0*er_slab;
 mur_slab = 2 - 1j*.1;
 mu_slab = mu0*mur_slab;
-kx = zeros(length(theta),length(e_slab));
+kx = zeros(length(theta),length(e_slab)+1);
 for i = 1:length(theta)
     for j = 1:length(e_slab)
             kx(i,j) = k0*sqrt(mu_slab*e_slab(j) - sin(theta(i))^2);        
     end
 end
-% kx = k0*sqrt(mu_slab*e_slab - sin(theta).^2);
-%% FEM
-R = zeros(length(theta),length(e_slab));
-eta = zeros(length(theta),length(e_slab));
-R(:,1) = -1;
+kx(:,end) = k0*sqrt(mu0*eps0-sin(theta).^2);
 
+%% FEM
+R = zeros(length(theta),n);
+eta = zeros(length(theta),n);
+R(:,1) = -1;
+% Compute ETA_m 
 for i = 1:length(theta)
-    for j = 2:length(e_slab)
-        eta(i,j) = (mu_slab*kx(i,j) - mu_slab*kx(i,j-1))...
-            /(mu_slab*kx(i,j) + mu_slab*kx(i,j-1));       
+    for j = 2:n
+        if j == length(e_slab)
+            eta(i,j) = (mu_slab*kx(i,j) - mu0*kx(i,j-1))...
+                /(mu_slab*kx(i,j) + mu0*kx(i,j-1));         
+        else
+            eta(i,j) = (kx(i,j) - kx(i,j-1))...
+                /(kx(i,j) + kx(i,j-1));   
+        end
+%         eta(i,j) = (mu_slab*kx(i,j) - mu_slab*kx(i,j-1))...
+%             /(mu_slab*kx(i,j) + mu_slab*kx(i,j-1));       
     end
 end
+% Compute R_m
 for i = 1:length(theta)
-    for j = 2:length(e_slab)
+    for j = 2:n
         
            R(i,j) = exp(2*1j*kx(i,j)*x(j))*...
             ((eta(i,j) + R(i,j-1)*exp(-2*1j*kx(i,j-1)*x(j)))...
@@ -42,7 +52,7 @@ for i = 1:length(theta)
 end
 
 subplot(1,2,1)
-plot(x/L,abs(e_slab)); title('Permitivity profile in slab');
+plot(y/L,abs(e_slab)); title('Permitivity profile in slab');
 ylabel('permitivity');xlabel('distance from PEC (x/L)');
 subplot(1,2,2)
 plot(theta*180/pi,abs(R(:,end)))
