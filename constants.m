e347414490b9f7edff373eabf5c29dd359f02dc0
Blake Levy <blake.a.levy@gmail.com>
2013-04-10 11:02:40 -0500

    Added analytic solution
    R and ETA calculation

--- constants.m
+++ constants.m
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 % example script
 %% Project #1
 % Authors: Adedayo Lawal and Blake Levy
-clc;clear;
-=======
 % constants.m
 %% Project #2
 % Authors: Blake Levy and Adedayo Lawal
->>>>>>> Added FEM and constants.m
 %% Set up Parameters
 c = 299792458;
 mu = (4*pi)*1e-7;
 epsilon = 1/(mu*c^2);
-<<<<<<< HEAD
-e_top = epsilon;
-e_bottom = 3*epsilon;
-=======
->>>>>>> Added FEM and constants.m
+eps0 = epsilon;
+freq = 1e6; % initial frequency
+lamb0 = c/f;
